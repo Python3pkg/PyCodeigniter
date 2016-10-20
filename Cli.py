b3ebb64dbbbb8ad82b32aeb44c7465becd9bbe4f
s-jqzhang <s-jqzhang@meizu.com>
2016-10-20 11:10:45 +0800

    task_id

--- Cli.py
+++ Cli.py
@@ -346,7 +346,7 @@ class Cli:
                 print(er)
                 ci.logger.error(er)
                 pass
-        ci.logger.info("task_id:%s,%index:%s,ip:%s,result:\n%s"%(data['task_id'], str(data['index']), data['ip'],data['result']))
+        ci.logger.info("task_id:%s,index:%s,ip:%s,result:\n%s"%(data['task_id'], str(data['index']), data['ip'],data['result']))
 
     def uuid(self,req,resp):
         return ci.uuid()
